6b7109b6e45f15e227254ad458ddd78514134898
tobiashillen <tobias.hillen@gmail.com>
2017-01-12 15:11:49 +0100

    Initial Commit

---
+++ ColorSlider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorSlider
+//
+//  Created by Tobias Hillén on 2017-01-12.
+//  Copyright © 2017 Tobias Hillén. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorSlider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ColorSlider
+//
+//  Created by Tobias Hillén on 2017-01-12.
+//  Copyright © 2017 Tobias Hillén. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
